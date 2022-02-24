408f4506a15b3cfcde2e0d32f2a7896de70bdc3e
ErwingForeroXpert <erwing.forero@xpertgroup.co>
2022-02-23 19:38:15 -0500

    Regular auto-commit at 19:38:15 on 23/02/2022

--- budget/afo/afo.py
+++ budget/afo/afo.py
@@ -326,8 +326,9 @@ class AFO(dfo):
         not_assign = agg_base[(~(mask_sectors | mask_assign_negatives)) & mask_not_assign]  # delete invalid sectors or assign negatives and not assigment
         assign = agg_base[(~(mask_sectors | mask_assign_negatives)) & (~mask_not_assign)] # delete invalid sectors or assign negatives and assigment
         assign_negative = agg_base[mask_assign_negatives] #save assign with negative values
-        agg_base = agg_base[~(mask_sectors | mask_assign_negatives)].reset_index(drop=True, inplace=True) # delete invalid sectors or assign negatives
-
+        agg_base = agg_base[~(mask_sectors | mask_assign_negatives)] # delete invalid sectors or assign negatives
+        agg_base.reset_index(drop=True, inplace=True)
+        
         # agrupation about "Ventas asignadas positivas"
         total_sales = assign.groupby(
             columns_level, as_index=False).agg(**aggregations[0])
